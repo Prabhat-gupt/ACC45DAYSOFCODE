6fa0a34e5f4095f47b7e5ab380158c12e31b4229
Prabhat Gupta <98599890+Prabhat-gupt@users.noreply.github.com>
2022-11-04 23:46:21 +0530

    Add files via upload

---
+++ Day15LargestString.java
@@ -0,0 +1,14 @@
+
+public class Day15LargestString {
+    public static void main(String[] args) {
+        String fruits[] = {"apple","banana","mango"};
+        String largest = fruits[0];
+        for(int i=1;i<fruits.length;i++){
+            if(largest.compareTo(fruits[i])<0){
+                largest = fruits[i];
+            }
+        }
+
+        System.out.println(largest);
+    }
+}
