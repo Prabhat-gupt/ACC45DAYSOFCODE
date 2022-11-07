2d4f416f6498c7561612fcc9ecf11c15160f07c2
Prabhat Gupta <98599890+Prabhat-gupt@users.noreply.github.com>
2022-11-07 23:55:48 +0530

    Add files via upload

---
+++ Day18ChocolaProblem.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Day18ChocolaProblem {
+    public static void main(String[] args) {
+        int n = 4, m = 6;
+        Integer costver[] = {2,1,3,1,4};
+        Integer costhor[] = {4,1,2};
+
+        Arrays.sort(costver,Collections.reverseOrder());
+        Arrays.sort(costhor,Collections.reverseOrder());
+
+        int h = 0,v = 0;
+        int hp = 1, vp = 1;
+        int cost = 0;
+
+        while(h<costhor.length && v<costver.length){
+            if(costver[v] <= costhor[v]){
+                cost += (costhor[h] * vp);
+                hp++;
+                h++;
+            }else{
+                cost += (costver[v] * hp);
+                vp++;
+                v++;    
+            }
+        }
+
+        while(h < costhor.length){
+            cost += (costhor[h] * vp);
+            hp++;
+            h++;
+        }
+
+        while(v < costver.length){
+            cost += (costver[v] * hp);
+            vp++;
+            v++;
+        }
+
+        System.out.println("Minimum cost of cuts = "+cost);
+    }
+}
